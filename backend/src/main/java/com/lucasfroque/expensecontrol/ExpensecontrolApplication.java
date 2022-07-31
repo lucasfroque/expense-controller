6b152c3ad090e2307a8da8fdf70d84e990d1fd84
Lucas Fernando <lucasfernandolf38@gmail.com>
2022-07-31 14:10:08 -0300

    Initial commit

---
+++ backend/src/main/java/com/lucasfroque/expensecontrol/ExpensecontrolApplication.java
@@ -0,0 +1,13 @@
+package com.lucasfroque.expensecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensecontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensecontrolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lucasfroque/expensecontrol/ExpensecontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasfroque.expensecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensecontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
